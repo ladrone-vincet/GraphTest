af11287690a37c14b7205fc6c7b7b0d902e2a8e9
Ladrone Vincet <mateusz77488@gmail.com>
2017-08-22 02:23:55 +0200

    MongoDB deleting

--- db-connector.js
+++ db-connector.js
@@ -3,9 +3,6 @@ const {MongoClient} = require('mongodb')
 const MONGO_URL = 'mongodb://localhost:27017/gqlTest'
 
 module.exports = async () => {
-  const db = await MongoClient.connect(MONGO_URL)//, (err, db) => {
-  // err ? console.warn(err + ' - - - - ERROR!') : console.log('DB connected - - - - GREAT!')
-  // })
-  // console.log('Log  ' + await db.collection('todos').find())
+  const db = await MongoClient.connect(MONGO_URL) // Validation?
   return {TODOs: db.collection('todos')}
 }
--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 // require('babel-register')({only: '/frontend/,'});
-// const graphql = require('graphql').graphql
 const buildSchema = require('graphql').buildSchema
 const express = require('express')
 const graphQLHTTP = require('express-graphql')
@@ -12,19 +11,9 @@ const Root = require('./resolvers')
 // const Schema = require('./schema.js')
 // const Component = require('frontend/componenet.jsx')
 
-// var ExTodos = [{
-//   'id': 1446739542,
-//   'title': 'Read emails',
-//   'completed': false
-// }, {
-//   'id': 1412740883,
-//   'title': 'Buy orange',
-//   'completed': true
-
-// }]
 const start = async () => {
   const mongo = await connectMongo()
-  console.log("Mongo - " + mongo.TODOs.find().forEach((item) => console.log(item)))
+  // console.log("Mongo - " + mongo.TODOs.find().forEach((item) => console.log(item)))
   // await mongo.insert(ExTodos)
   const app = express()
     // .use('/', graphQLHTTP({schema: Schema, graphiql: true, pretty: true} ))
@@ -35,13 +24,7 @@ const start = async () => {
       graphiql: true,
       pretty: true}))
     .listen(8080, function (err) { err ? console.warn(err) : console.log('GraphQL is now running on localhost:8080') })
-    console.log('Started')
+  console.log('Started')
 }
 
 start()
-
-// const query = 'query { todos { id, title, completed} }'
-// graphql(Schema, query, Root).then(function (result) {
-//   console.log(JSON.stringify(result))
-//   console.log(JSON.stringify(result.data.todos[0].title))
-// })
